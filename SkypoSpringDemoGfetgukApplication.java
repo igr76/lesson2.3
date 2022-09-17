4d34ade410b7d5d06f786c28bfef6efb768e5925
igr1 <dfrgth>
2022-09-17 20:22:59 +1000

    2.3 начало

---
+++ SkypoSpringDemoGfetgukApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skypospringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkypoSpringDemoGfetgukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkypoSpringDemoGfetgukApplication.class, args);
+	}
+
+}
